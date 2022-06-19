88daf67c8342e6f155f39e2c9dc0651ec3ed2ee3
mhidalgo94 <marito.hidalgo94@gmail.com>
2022-06-19 02:34:05 -0400

    Add requirements and parse str to dict data

--- console_requests.py
+++ console_requests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import ast
 import urllib
 import argparse
 
@@ -27,7 +28,11 @@ class Fast_Requests:
         return self.args.p
 
     def get_data(self):
-        return self.args.d
+        if self.args.d:
+            d = ast.literal_eval(self.args.d)
+        else:
+            d = self.args.d
+        return d
 
     def get_headers(self):
         return self.args.hds or {}
@@ -57,44 +62,47 @@ class Fast_Requests:
         method = self.METHOD
         params=self.params
         headers = self.headers
+        # try:
         if method == 'GET':
             r = requests.get(self.HOST, params=params,headers=headers)
             return r
         elif method == "POST":
-            d = json.dumps(self.DATA.replace("\'", "\""))
             headers["Content-Type"] = "application/json"
-            r = requests.post(self.HOST, data = json.loads(d),headers=headers,params=params)   #, headers={"Content-Type":"application/json"})
+            r = requests.post(self.HOST, data= json.dumps(self.DATA),headers=headers,params=params)   #, headers={"Content-Type":"application/json"})
             return r
         elif method == "PUT":
-            d = json.dumps(self.DATA.replace("\'", "\""))
             headers["Content-Type"] = "application/json"
-            r = requests.put(self.HOST, data = json.loads(d),headers=headers,params=params) 
+            r = requests.put(self.HOST, data = json.dumps(self.DATA),headers=headers,params=params) 
             return r
         elif method == "DELETE":
             r = requests.delete(self.HOST,params=params)  
             return r
+        # except Exception as e:
+        #     print(e)
             
     def __repr__(self) -> str:
-        data = self.args.f
+        data = self.args.format
         try:
             if data == "json":
-                return f'{self.response.json()}'
+                return f'STATUS_CODE:{self.response.status_code}- {self.response.json()}'
             elif data == "content":
-                return f'{self.response.content}'
+                return f'STATUS_CODE:{self.response.status_code}- {self.response.content}'
             elif data == "text":
-                return f'{self.response.text}'
+                return f'STATUS_CODE:{self.response.status_code}- {self.response.text}'
         except json.JSONDecodeError:
             return f'{self.response.text}'
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u",type=str, default="http://localhost",help="Servidor destino.")
-    parser.add_argument("-m",type=str, default="GET",help="Metodo de peticion.")
-    parser.add_argument("-f",type=str, default="json",help="Formato retorno de datos.")
-    parser.add_argument("-d",type=str, help="Datos para enviar (POST, DELETE, PUT).")
+    parser.add_argument("-format",type=str, default="json",help="Formato retorno de datos.")
+    parser.add_argument("-d",type=str,default=None, help="Datos para enviar (POST, DELETE, PUT).")
     parser.add_argument("-p",type=str, default=None, help="Parametros para enviar.")
     parser.add_argument("-hds",type=str, default=None, help="Headers para enviar.")
+    parser.add_argument("-u",type=str, required=True,help="Servidor destino.")
+    parser.add_argument("-m",type=str, default="GET",help="Metodo de peticion.")
+
+
 
 
     args = parser.parse_args()
